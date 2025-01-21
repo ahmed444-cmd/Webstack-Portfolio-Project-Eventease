1032ae70da48eb9c1cdb84048ba5a9ba589a8d4f
El Abid Ahmed <ahmedelabide@gmail.com>
2025-01-21 21:06:31 +0100

    fonts

---
+++ my-app/src/fontawesome.js
@@ -0,0 +1,40 @@
+// Import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Import icons
+import { 
+    faSun, 
+    faMoon, 
+    faStar, 
+    faChevronDown,
+    faLanguage,
+    faBars,
+    faTimes,
+    faCalendar,
+    faMapMarker,
+    faClock,
+    faEnvelope,
+    faPhone,
+    faUser,
+    faCheck,
+    faExclamationCircle
+} from '@fortawesome/free-solid-svg-icons';
+
+// Add icons to library
+library.add(
+    faSun,
+    faMoon,
+    faStar,
+    faChevronDown,
+    faLanguage,
+    faBars,
+    faTimes,
+    faCalendar,
+    faMapMarker,
+    faClock,
+    faEnvelope,
+    faPhone,
+    faUser,
+    faCheck,
+    faExclamationCircle
+);
